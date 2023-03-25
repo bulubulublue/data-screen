b69cc0732e6e0e67f67861dbe61ff1073b429fb9
ChenCheng-0715 <bendelayoc@gmail.com>
2023-03-26 00:00:49 +0800

    feat: 添加header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,3 +1,47 @@
 <template>
-  <div class="view-container">header</div>
+  <div class="view-container">
+    <div class="logo-container">
+      <logo></logo>
+    </div>
+    <div class="title-container">
+      <div class="title-cn">外卖业务数据大盘</div>
+      <div class="title-en">Delivery Overview</div>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style scoped lang="scss">
+.view-container {
+  background-color: rgb(31, 26, 32);
+  display: flex;
+  align-items: center;
+  padding: 0 64px;
+
+  .logo-container {
+    height: 120px;
+    width: 120px;
+    margin-right: 20px;
+  }
+
+  .title-container {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    .title-cn {
+      font-weight: bold;
+      font-size: 60px;
+      letter-spacing: 5px;
+    }
+
+    .title-en {
+      font-size: 35px;
+      letter-spacing: 3px;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import icon from 'rollup-datav-libs/src/components/Icon/index'
 import Loading from 'rollup-datav-libs/src/components/Loading/index'
 import Flybox from 'rollup-datav-libs/src/components/Flybox/index'
 import Container from 'rollup-datav-libs/src/components/Container/index'
-
+import Logo from 'rollup-datav-libs/src/components/Logo/index'
+import CountTo from 'rollup-datav-libs/src/components/CountTo/index'
 const app = createApp(App)
 app.use(router)
 app.use(icon)
 app.use(Loading)
 app.use(Flybox)
 app.use(Container)
+app.use(Logo)
+app.use(CountTo)
 app.mount('#app')
